3da75b39a912e58a90dc3df70800e97cc65adc3b
dev-299792 <devgagrani123@gmail.com>
2025-07-17 23:12:38 +0530

    add in memory user login. closes #1

---
+++ authserver/src/main/java/com/example/authserver/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.authserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    String home() {
+        return "home";
+    }
+}
