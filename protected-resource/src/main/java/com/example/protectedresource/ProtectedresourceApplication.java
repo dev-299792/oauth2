bb5f2c23527ed9595f56e808a2de1fa9ec78d95a
dev-299792 <devgagrani123@gmail.com>
2025-08-17 18:55:25 +0530

    Added spring project for protected resource, closes #40.

---
+++ protected-resource/src/main/java/com/example/protectedresource/ProtectedresourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.protectedresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectedresourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectedresourceApplication.class, args);
+	}
+
+}
---
+++ protected-resource/src/test/java/com/example/protectedresource/ProtectedresourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protectedresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtectedresourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
